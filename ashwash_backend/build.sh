a1220326b1dee11a1abb242757d2586ef1ce8144
joba967 <jobaacharjee5@gmail.com>
2026-08-05 01:05:28 +0600

    Auto seed database on build

--- ashwash_backend/build.sh
+++ ashwash_backend/build.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-# exit on error
+# Exit on error
 set -o errexit
 
 pip install -r requirements.txt
-
 python manage.py collectstatic --no-input
 python manage.py migrate
+python seed_db.py
