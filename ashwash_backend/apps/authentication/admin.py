c97c19dc4fb60fc90fc2f6c1e38d18e9955de439
joba967 <jobaacharjee5@gmail.com>
2026-08-05 21:33:41 +0600

    Use UserAdmin for User model to enable password hashing in admin

--- ashwash_backend/apps/authentication/admin.py
+++ ashwash_backend/apps/authentication/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import User, Category, PatientProfile, SpecialistProfile
 
-admin.site.register(User)
+class CustomUserAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (
+        ('Extra Info', {'fields': ('role', 'phone_number', 'profile_picture', 'preferences', 'selected_categories')}),
+    )
+    list_display = ('username', 'email', 'first_name', 'last_name', 'role', 'is_staff')
+
+admin.site.register(User, CustomUserAdmin)
 admin.site.register(Category)
 admin.site.register(PatientProfile)
 admin.site.register(SpecialistProfile)
